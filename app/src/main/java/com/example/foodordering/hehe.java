032f8d2bbd68fede0a7089d65646b4bba81c9385
Anirudh-1149 <anirudh18@iitg.ac.in>
2020-09-25 18:24:46 +0530

    Create hehe.java

---
+++ app/src/main/java/com/example/foodordering/hehe.java
@@ -0,0 +1,24 @@
+package com.example.foodordering;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.foodordering.Models.Restaurant;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
